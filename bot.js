5ec422e5ea7944656e9c7d739208ec8620c46c8e
Dustin Hayre <dhayre@gmail.com>
2023-11-01 15:14:00 -0400

    2.0.1

--- bot.js
+++ bot.js
@@ -45,8 +45,8 @@ bot.on('ready', () =>
         // Get a list of members with Newbie role
         //const guest = bot.guild.members.fetch();
         const guild = bot.guilds.cache.get(newbID);
-        const members = guild.members.cache.get;
-        members.forEach(member => {
+        const newmembers = guild.members.cache.get;
+        newmembers.forEach(member => {
             if (bot.role.id === newbID)
             {
         //bot.guild.members.fetch(newbID).forEach(member =>
