5bdcd83c1a01d0d89054f1a4f18d7be920709ae0
Dustin Hayre <dhayre@gmail.com>
2023-11-01 15:28:15 -0400

    2.0.1

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const bot = new Client({
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers
+		GatewayIntentBits.GuildMembers,
 	],
 });
 
