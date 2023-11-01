80ef7e4ff17f08f3f7a993467d8e6122d6f1b025
Dustin Hayre <dhayre@gmail.com>
2023-11-01 14:16:09 -0400

    2.0.1

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ bot.on('ready', () =>
         const guild = bot.guilds.fetch(process.env.hostDiscordServer);
         const members = guild.members.cache;
         members.forEach(member => {
-            if oldMember.roles.cache.some(role => role.name === newbID)
+            if members.roles.cache.some(role => role.name === newbID)
             {
         //bot.guild.members.fetch(newbID).forEach(member =>
         //bot.guild.members.fetch({cache : false}).then(members=>{
