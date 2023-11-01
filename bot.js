722f0db4cc697407017289b710c636c161e2c50e
Dustin Hayre <dhayre@gmail.com>
2023-11-01 18:38:03 -0400

    2.0.1

--- bot.js
+++ bot.js
@@ -52,7 +52,7 @@ bot.on('ready', (rdy) =>
             // Get today's date
             const todaysDate = new Date(Date.now()).toLocaleString().split(',')[0];
             // Find member's join date
-            let person = bot.members.fetch(member);
+            let person = bot.guilds.cache.get(member);
             console.log(person);
 
             const joinDate = person.joinedAt;
