23932ac8dfbef6b75b10dbbe703efcc1e84d5fb6
Dustin Hayre <dhayre@gmail.com>
2023-11-01 12:22:45 -0400

    2.0.1

--- bot.js
+++ bot.js
@@ -109,7 +109,7 @@ bot.on('guildMemberAdd', (guildMember) =>
     member.roles.add(newbID);
     // Send a DM to the new user explaining our rules.
     const embed = new Discord.MessageEmbed().setTitle('Welcome to ***Charlie Company 337***').setDescription('***CC337 rules***');
-    member.send(embed)
+    member.send(embed);
     /* member.send(    
         {
         embed:
