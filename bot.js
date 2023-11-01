6e00b7caf06d1b1cb3a4e576dabda561ed3393c8
Dustin Hayre <dhayre@gmail.com>
2023-11-01 19:18:56 -0400

    2.0.1

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ bot.on('ready', (rdy) =>
             console.log('Member:' + mbmr);
             console.log('Member:' + (todaysDate - 3));
             console.log('Today: ' + (todaysDate - joinDate));
-            console.log('Joined: ' + (joinDate > (todaysDate - 8640000)));
+            console.log('Joined: ' + (joinDate - todaysDate));
             // Add three days to member's join date
             //const threeDaysAfterJoinDate = joinDatejoinDate.getDate() + 3);
 
