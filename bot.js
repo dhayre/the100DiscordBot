337c211bb049299c23db81ca0a579d2c43a0b169
Dustin Hayre <dhayre@gmail.com>
2023-11-01 19:25:05 -0400

    2.0.1

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ bot.on('ready', (rdy) =>
 	            .setFooter({ text: `If you have any questions, please let a member of the leadership team know or post in ${ bot.channels.cache.get(welcomeChannelID) } for help`, iconURL: 'http://i.imgur.com/KslihqE.png' });
      
 
-            if (joinDate < todaysDate - 3 && !member.roles.caxche.has(gruntID))
+            if (todaysDate - joinDate < 259200000 && member.roles.cache.has(newbID))
             {
                 console.log('Would kick ' + member.name)
                 // Generate a unique, single use, 7 day invite for the member and send them a DM
